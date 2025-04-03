77e9fccd99cb20c473a09290fab3928234441c82
Gabriel <gabicjs87@gmail.com>
2025-04-02 23:08:53 -0300

    se agregaron comentarios a ejercicio 1

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,6 @@
+#Se define variable nombre
 nombre = "Gabriel"; 
+#Se define variable apellido
 apellido = " Torres"; 
 print(f"Hola soy {nombre}{apellido} alumno de la tecnicatura en Programacion"); 
 
